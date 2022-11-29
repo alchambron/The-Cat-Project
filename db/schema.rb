cb6b45e8093e749fad424e7091fc780156672c17
thimibka <fseetp@gmail.com>
2022-11-29 20:29:45 +0100

    page show ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_165516) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_214542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,13 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_165516) do
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
-  end
-
-  create_table "carts", force: :cascade do |t|
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
