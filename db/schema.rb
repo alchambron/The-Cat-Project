40268f5581a2bc6b29c12d9b12fb5f1290b48eee
anthonymiloradovic <anthony.miloradovic@icloud.com>
2022-11-29 15:52:53 +0100

    changement index3

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_204152) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
