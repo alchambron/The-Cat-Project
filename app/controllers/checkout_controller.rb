336bf3c2671eab8ed6e47e03beb5fe89c805a5d1
Thomas Raducha <thomasraducha@gmail.com>
2022-11-30 15:29:39 +0100

    controller checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,5 @@
 class CheckoutController < ApplicationController
-    #create session stripe
+  # create session stripe
   def create
     @user = current_user
     @total = params[:total].to_d
@@ -8,30 +8,29 @@ class CheckoutController < ApplicationController
       line_items: [
         {
           name: 'Rails Stripe Checkout',
-          amount: (@total*100).to_i,
+          amount: (@total * 100).to_i,
           currency: 'eur',
           quantity: 1
-        },
+        }
       ],
-      succes_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}'
+      succes_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: checkout_cancel_url
     )
     respond_to do |format|
-        #renders create.js.erb
-        format.js 
+      # renders create.js.erb
+      format.js
     end
   end
 
-  def success                            
-    #extrait moi les infos de la session
+  def success
+    # extrait moi les infos de la session
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
-    #extrait moi les infos par rapport au montant qui a réelement payé lors de cette session
+    # extrait moi les infos par rapport au montant qui a réelement payé lors de cette session
     @payment_intent = Stripe::PaymentIntent.retrieve
   end
 
-  def cancel                            
+  def cancel
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve
   end
-
 end
