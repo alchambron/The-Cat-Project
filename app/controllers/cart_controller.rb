79d4a77cd27e0c0ecd4d2edb6efb0c80107fb1b2
Thomas Raducha <thomasraducha@gmail.com>
2022-12-03 14:04:18 +0100

    Add cart class

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,27 +1,15 @@
-require 'pry'
 class CartController < ApplicationController
   def list_items
-    @items = []
-    return if session[:cart].nil? || session[:cart].length == 0
-
-    # On récupère depuis la base tous les items correspondants à ceux ajoutés au panier
-    @items = Item.find(session[:cart])
+    @items = @cart.items
   end
 
   def add_item
-    return if params[:id].nil?
-
-    # Si le panier n'a pas encore été initialisé, on l'initialise
-    session[:cart] = [] if session[:cart].nil?
-    # On ajoute l'item_id voulu à la liste déjà existante (ou pas) dans le panier
-    session[:cart] << params[:id]
+    get_cart.add_item(params[:id])
   end
 
   def delete_item
-    return if params[:id].nil?
-
-    # On supprimer l'item_id voulu de la liste du panier
-    session[:cart] = session[:cart].reject { |i| i == params[:id] }
+    get_cart.delete_item(params[:id])
+    # Une fois que l'item a été retiré du panier, on recharge la page appelante pour que les items soient à jour
     redirect_back(fallback_location: root_path)
   end
 end
